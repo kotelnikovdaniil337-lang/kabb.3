280e241f9f228e0a5f2d7c31358d5464e183c6f1
Daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-05-14 09:55:55 +0000

    Сортировка массива пузырьком в порядке возрастания.

--- KotelnikovD.cpp
+++ KotelnikovD.cpp
@@ -23,3 +23,12 @@ int main() {
         cout << arr[i] << " ";
     }
     cout << endl;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
