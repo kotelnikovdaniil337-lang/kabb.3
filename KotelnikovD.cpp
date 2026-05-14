0a4fcec834799fbc6cf5b9740650b0e4ad1a6729
Daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-05-14 10:19:25 +0000

    Ручное заполнение матриц с контролем корректности ввода

--- KotelnikovD.cpp
+++ KotelnikovD.cpp
@@ -8,3 +8,21 @@ int main() {
     int choice;
     cin >> choice;
     if (choice == 1) {
+         cout << "Введите матрицу A (9 чисел): ";
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                cin >> A[i][j];
+        cout << "Введите матрицу B (9 чисел): ";
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                cin >> B[i][j];
+    } else {
+        double testA[N][N] = {{1, 2, 3}, {0, 1, 4}, {5, 6, 0}};
+        double testB[N][N] = {{2, 0, 1}, {3, 1, 0}, {1, 2, 1}};
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                A[i][j] = testA[i][j];
+                B[i][j] = testB[i][j];
+            }
+        cout << "Использованы готовые матрицы\n";
+    }
