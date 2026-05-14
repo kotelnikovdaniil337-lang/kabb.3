e3b7c226804797d20725d3f69f2c0638212ac38c
Daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-05-14 10:17:42 +0000

    Выбор способа ввода матриц: ручной или готовый (тестовый)

--- KotelnikovD.cpp
+++ KotelnikovD.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 int main() {
-    const int N = 3; // Фиксированный размер матрицы 3x3
+    const int N = 3;
     double A[N][N], B[N][N], C[N][N];
      cout << "1 — Ручной ввод, 2 — Готовые матрицы. Выберите: ";
     int choice;
     cin >> choice;
-
     if (choice == 1) {
