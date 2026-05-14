8f15d1237424021d99ad1913506b4f8ab531f777
Daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-05-14 11:38:34 +0000

    Ввод величины орт для обоих векторов.

--- KotelnikovD.cpp
+++ KotelnikovD.cpp
@@ -1,76 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <random>
 using namespace std;
 
 int main() {
-    int n, choice;
-    vector<double> arr, filtered;
-    cout << "=== Расчёт СКО и фильтрация массива ===\n";
-    cout << "1 — Ручной ввод\n2 — Случайная генерация\nВыберите: ";
-    cin >> choice;
-    cout << "Размер массива: ";
-    while (!(cin >> n) || n <= 0) {
-        cin.clear();
-        cin.ignore(1000, '\n');
-        cout << "Ошибка! Введите положительное число: ";
-    }
-    if (choice == 1) {
-        cout << "Введите " << n << " элементов:\n";
-        for (int i = 0; i < n; i++) {
-            cout << "Элемент " << i + 1 << ": ";
-            while (!(cin >> arr[i])) {
-                cin.clear();
-                cin.ignore(1000, '\n');
-                cout << "Ошибка! Введите число: ";
-            }
-        }
-    } else {
-        random_device rd;
-        mt19937 gen(rd());
-        uniform_real_distribution<double> dist(0.0, 100.0);
-
-        for (int i = 0; i < n; i++) {
-            arr.push_back(dist(gen));
-        }
-    }
-    cout << "\nИсходный массив:\n";
-    for (double x : arr) {
-        cout << x << " ";
-    }
-    cout << endl;
-    double sum = 0;
-    for (double x : arr) sum += x;
-    double mean = sum / n;
-    double variance = 0;
-    for (double x : arr) {
-        variance += (x - mean) * (x - mean);
-    }
-    variance /= n; 
-    double std_dev = sqrt(variance);
-    cout << "\nСреднее значение: " << mean << endl;
-    cout << "СКО: " << std_dev << endl;
-     double lower_bound = mean - std_dev;
-    double upper_bound = mean + std_dev;
-
-    for (double x : arr) {
-        if (x >= lower_bound && x <= upper_bound) {
-            filtered.push_back(x);
-        }
-    }
-     cout << "\nОтфильтрованный массив (в пределах ±1 СКО):\n";
-    if (filtered.empty()) {
-        cout << "Все элементы удалены\n";
-    } else {
-        for (double x : filtered) {
-            cout << x << " ";
-        }
-        cout << endl;
-    }
-    cout << "\nНажмите Enter для выхода...";
-    cin.ignore();
-    cin.get();
-
-    return 0;
-}
+    int n;
+    vector<double> v1, v2, v1_norm, v2_norm;
+    cout << "=== Расчёт угла между векторами ===\n";
+    cout << "Введите размерность векторов: ";
+    cin >> n;
