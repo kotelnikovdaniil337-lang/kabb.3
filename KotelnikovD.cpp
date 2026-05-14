1beda948004503a24690c46335d9eb5301bb34a8
Daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-05-14 10:20:25 +0000

    Вывод исходных матриц в консоль

--- KotelnikovD.cpp
+++ KotelnikovD.cpp
@@ -26,3 +26,15 @@ int main() {
             }
         cout << "Использованы готовые матрицы\n";
     }
+    cout << "\nМатрица A:\n";
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << A[i][j] << " ";
+        cout << endl;
+    }
+    cout << "\nМатрица B:\n";
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << B[i][j] << " ";
+        cout << endl;
+    }
